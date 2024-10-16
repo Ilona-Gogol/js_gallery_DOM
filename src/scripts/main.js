82111c3b5a25db05c20da56e90a96d06d9c169d8
Ilona Gogol <ilonka.gogol@gmail.com>
2024-10-16 16:02:49 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+const listItem = document.querySelectorAll('#thumbs a');
+
+listItem.forEach((thumb) => {
+  thumb.addEventListener('click', () => {
+    const newSrc = thumb.getAttribute('href');
+
+    largeImg.src = newSrc;
+  });
+});
