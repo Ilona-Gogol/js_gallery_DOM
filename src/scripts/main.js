f3c8f118972fca78c497270ce382e37420025b68
Ilona <168419113+Ilona-Gogol@users.noreply.github.com>
2024-10-16 16:15:25 +0300

    Update AI

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,6 +5,8 @@ const listItem = document.querySelectorAll('#thumbs a');
 
 listItem.forEach((thumb) => {
   thumb.addEventListener('click', () => {
+    event.preventDefault(); 
+
     const newSrc = thumb.getAttribute('href');
 
     largeImg.src = newSrc;
